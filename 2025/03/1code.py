72ad030ada547ed9ee6f20d5ac2d470864791318
Sandy F <sandyforrest001@outlook.com>
2025-11-23 23:38:00 +0000

    2025 day 3 part 1

--- 2025/03/1code.py
+++ 2025/03/1code.py
@@ -6,3 +6,14 @@ _filename = __file__.replace('code.py', 'notes')
 
 with open(_filename, 'r') as infile:
     notes = infile.readlines()
+
+crates = []
+for i in notes[0].split(','): crates.append(int(i))
+crates.sort()
+
+for i in crates:
+    while crates.count(i) > 1:
+        crates.pop(crates.index(i))
+
+print(sum(i for i in crates))
+pass
